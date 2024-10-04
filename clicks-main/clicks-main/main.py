a51f3c25b619d7f539f3f2cca9dac499e7c3ffe0
4moree <46574507+4moree@users.noreply.github.com>
2024-10-04 16:13:25 +0300

    update

--- clicks-main/clicks-main/main.py
+++ clicks-main/clicks-main/main.py
@@ -1,53 +1,51 @@
-from urllib.parse import urlparse, urlsplit
-
 import requests
+from urllib.parse import urlsplit
 
-token = 'd70059fdd70059fdd70059fdb7d41feed4dd700d70059fdb1fe855c86567060d246adc4'
+
+TOKEN = 'd70059fdd70059fdd70059fdb7d41feed4dd700d70059fdb1fe855c86567060d246adc4'
 url = input('Введите ссылку: ')
 
-def shorten_link(token, url):
-    vk_url = 'https://api.vk.ru/method/utils.getShortLink'
+def short_link(TOKEN, url):
+    vk_api_url = 'https://api.vk.ru/method/utils.getShortLink'
     try:
         payload = {
-            'access_token': token,
+            'access_token': TOKEN,
             'v': '5.199',
             'url': url
             }
 
-        response = requests.get(vk_url, params=payload)
+        response = requests.get(vk_api_url, params=payload)
         response.raise_for_status()
         data = response.json()
         return data['response']['short_url']
     except KeyError:
         return 'Неверная ссылка'
 
-def count_clicks(token):
-    vk_url = 'https://api.vk.ru/method/utils.getLinkStats'
-
+def count_clicks(TOKEN):
+    vk_api_url = 'https://api.vk.ru/method/utils.getLinkStats'
     try:
-        parsed_split = urlsplit(shorten_link(token, url))
-        key_parsed = parsed_split.path.split('/')[-1]
+        parsed_split = urlsplit(short_link(TOKEN, url))
+        key_url_parsed = parsed_split.path.split('/')[-1]
         payload = {
-            'access_token': token,
+            'access_token': TOKEN,
             'v': '5.199',
-            'key': key_parsed,
+            'key': key_url_parsed,
             'interval': 'forever',
             'extended': 0
         }
-        response = requests.get(vk_url, params=payload)
+        response = requests.get(vk_api_url, params=payload)
         response.raise_for_status()
         data = response.json()
-        views = data['response']['stats'][0]['views']
-        return views
+        views_url = data['response']['stats'][0]['views']
+        return views_url
     except KeyError:
         return 'Ошибка сокращенной ссылки'
+    except IndexError: # Новая ссылка без переходов.
+        return '0'
 
 
-print(shorten_link(token,url))
-print('Количество переходов по ссылке: ', count_clicks(token))
-
-
-
+print('Сокращенная ссылка:', short_link(TOKEN,url))
+print('Количество переходов по ссылке:', count_clicks(TOKEN))
 
 
 
