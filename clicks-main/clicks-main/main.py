4b8c363a2e13d244346e1f57c4d8e122dfb0cfb8
4moree <46574507+4moree@users.noreply.github.com>
2024-10-04 14:58:24 +0300

    update

--- clicks-main/clicks-main/main.py
+++ clicks-main/clicks-main/main.py
@@ -1,52 +1,57 @@
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlsplit
 
 import requests
 
+token = 'd70059fdd70059fdd70059fdb7d41feed4dd700d70059fdb1fe855c86567060d246adc4'
+url = input('Введите ссылку: ')
 
 def shorten_link(token, url):
     vk_url = 'https://api.vk.ru/method/utils.getShortLink'
-    payload = {
-        'access_token': token,
-        'v': '5.199',
-        'url': url
-        }
     try:
+        payload = {
+            'access_token': token,
+            'v': '5.199',
+            'url': url
+            }
+
         response = requests.get(vk_url, params=payload)
         response.raise_for_status()
         data = response.json()
-        value1 =  data.get('response')
-        short_url = value1.get('short_url')
-        key_url = value1.get('key')
-        return short_url, key_url
-    except:
-        print('Ошибка, неверный адрес')
-    return None, None
-
+        return data['response']['short_url']
+    except KeyError:
+        return 'Неверная ссылка'
 
 def count_clicks(token):
     vk_url = 'https://api.vk.ru/method/utils.getLinkStats'
 
     try:
+        parsed_split = urlsplit(shorten_link(token, url))
+        key_parsed = parsed_split.path.split('/')[-1]
         payload = {
-        'access_token': token,
-        'v': '5.199',
-        'key': short_key,
-        'extended': '0'
+            'access_token': token,
+            'v': '5.199',
+            'key': key_parsed,
+            'interval': 'forever',
+            'extended': 0
         }
         response = requests.get(vk_url, params=payload)
         response.raise_for_status()
         data = response.json()
-        return data['response']['stats']
-    except:
-        print('Ошибка, неверный ключ')
-        return None
+        views = data['response']['stats'][0]['views']
+        return views
+    except KeyError:
+        return 'Ошибка сокращенной ссылки'
 
 
-token = 'd70059fdd70059fdd70059fdb7d41feed4dd700d70059fdb1fe855c86567060d246adc4'
-url = input('Введите ссылку: ')
-short_url, short_key = shorten_link(token,url)
-print(short_url)
-print(count_clicks(token))
+print(shorten_link(token,url))
+print('Количество переходов по ссылке: ', count_clicks(token))
+
+
+
+
+
+
+
 
 
 
