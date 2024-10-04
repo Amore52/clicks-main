dcb6a25fe80a8757b39ca497bb8b64b472d9ee95
4moree <46574507+4moree@users.noreply.github.com>
2024-10-04 22:39:31 +0300

    update

--- clicks-main/clicks-main/main.py
+++ clicks-main/clicks-main/main.py
@@ -1,18 +1,21 @@
 import requests
 from urllib.parse import urlsplit
+from dotenv import load_dotenv
+import os
 
 
-TOKEN = 'd70059fdd70059fdd70059fdb7d41feed4dd700d70059fdb1fe855c86567060d246adc4'
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 url = input('Введите ссылку: ')
 
 def short_link(TOKEN, url):
-    vk_api_url = 'https://api.vk.ru/method/utils.getShortLink'
+    vk_api_url = 'https://api.vk.com/method/utils.getShortLink'
     try:
         payload = {
             'access_token': TOKEN,
             'v': '5.199',
             'url': url
-            }
+        }
 
         response = requests.get(vk_api_url, params=payload)
         response.raise_for_status()
@@ -21,10 +24,11 @@ def short_link(TOKEN, url):
     except KeyError:
         return 'Неверная ссылка'
 
-def count_clicks(TOKEN):
-    vk_api_url = 'https://api.vk.ru/method/utils.getLinkStats'
+
+def count_clicks(TOKEN, short_url):
+    vk_api_url = 'https://api.vk.com/method/utils.getLinkStats'
     try:
-        parsed_split = urlsplit(short_link(TOKEN, url))
+        parsed_split = urlsplit(short_url)
         key_url_parsed = parsed_split.path.split('/')[-1]
         payload = {
             'access_token': TOKEN,
@@ -40,22 +44,20 @@ def count_clicks(TOKEN):
         return views_url
     except KeyError:
         return 'Ошибка сокращенной ссылки'
-    except IndexError: # Новая ссылка без переходов.
+    except IndexError:  # Новая ссылка без переходов.
         return '0'
 
 
-print('Сокращенная ссылка:', short_link(TOKEN,url))
-print('Количество переходов по ссылке:', count_clicks(TOKEN))
+def is_shorten_link(url):
+    if 'vk.cc' in url:
+        clicks = count_clicks(TOKEN, url)
+        print('Количество переходов по ссылке:', clicks)
+    else:
+        short_url = short_link(TOKEN, url)
+        clicks = count_clicks(TOKEN, short_url)
+        print('Сокращенная ссылка:', short_url)
+        print('Количество переходов по ней:', clicks)
 
 
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    is_shorten_link(url)
