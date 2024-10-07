a155f6c73f73bf827c1d515314cd31b40b8f71c2
4moree <46574507+4moree@users.noreply.github.com>
2024-10-07 23:31:10 +0300

    add in main try except

---
+++ clicks-main/models.py
@@ -0,0 +1,59 @@
+import requests
+from urllib.parse import urlsplit
+
+
+
+def get_short_link(vk_token, url):
+    vk_api_url = 'https://api.vk.com/method/utils.getShortLink'
+    payload = {
+        'access_token': vk_token,
+        'v': '5.199',
+        'url': url
+    }
+
+    response = requests.get(vk_api_url, params=payload)
+    response.raise_for_status()
+    api_response = response.json()
+    return api_response['response']['short_url']
+
+
+
+def count_clicks(vk_token, short_url):
+    vk_api_url = 'https://api.vk.com/method/utils.getLinkStats'
+    parsed_split = urlsplit(short_url)
+    key_url_parsed = parsed_split.path.split('/')[-1]
+    payload = {
+        'access_token': vk_token,
+        'v': '5.199',
+        'key': key_url_parsed,
+        'interval': 'forever',
+        'extended': 0
+    }
+    response = requests.get(vk_api_url, params=payload)
+    response.raise_for_status()
+    api_response = response.json()
+    url_stats = api_response.get('response', {}).get('stats', [])
+    return url_stats
+
+
+
+def is_shorten_link(url):
+    return 'vk.cc' in url
+
+def get_link_info(vk_token, url):
+    if is_shorten_link(url) is True:
+        clicks = count_clicks(vk_token, url)
+        print('Количество переходов по ссылке:', clicks)
+
+    else:
+        short_url = get_short_link(vk_token, url)
+        clicks = count_clicks(vk_token, short_url)
+        print('Сокращенная ссылка:', short_url)
+        print('Количество переходов по ней:', clicks)
+
+
+
+
+
+
+
